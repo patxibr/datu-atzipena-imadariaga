20886982bac0b8cfbca2d1ee4c17b4dd1b8c3af5
barrutia.patxi <barrutia.patxi@UH-105-11.uni.lan>
2020-09-29 08:50:43 +0200

    Kalkulagailu sofistikatua gehitua

--- UD0/Kalkulagailua/src/pkg1/KalkulagailuaController.java
+++ UD0/Kalkulagailua/src/pkg1/KalkulagailuaController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -24,6 +25,34 @@ public class KalkulagailuaController implements Initializable {
     
     @FXML
     private TextField batugai1,batugai2,biderkagai1,biderkagai2;
+    @FXML
+    private Button batu;
+    @FXML
+    private Button biderkatu;
+    @FXML
+    private TextField kengai1;
+    @FXML 
+    private TextField kengai2;
+    @FXML
+    private TextField zatigai1;
+    @FXML
+    private TextField zatigai2;
+    @FXML
+    private Button kendu;
+    @FXML
+    private Button zatitu;
+    @FXML
+    private Label kenketa;
+    @FXML
+    private Label zatiketa;
+    @FXML
+    private TextField berregai1;
+    @FXML
+    private TextField berregai2;
+    @FXML
+    private Button berretu;
+    @FXML
+    private Label berreketa;
     
     @FXML
     private void handleBatuAction(ActionEvent event) {
@@ -41,5 +70,27 @@ public class KalkulagailuaController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void handleKenduAction(ActionEvent event) {
+        System.out.println("Kenketa egin dugu.");
+        kenketa.setText(""+(Double.parseDouble(kengai1.getText()) - Double.parseDouble(kengai2.getText())));
+    }
+
+    @FXML
+    private void handleZatituAction(ActionEvent event) {
+        System.out.println("Zatiketa egin dugu.");
+        zatiketa.setText(""+(Double.parseDouble(zatigai1.getText()) / Double.parseDouble(zatigai2.getText())));
+    }
+
+    @FXML
+    private void handleBerretuAction(ActionEvent event) {
+        System.out.println("Berreketa egin dugu.");
+        double j = 1;
+        for (double i = Double.parseDouble(berregai2.getText()); i>0; i--){
+            j = j*Double.parseDouble(berregai1.getText());
+        }
+        berreketa.setText(""+j);
+    }
     
 }
